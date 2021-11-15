cc9e75a694e054308678f84a46461d0ed5841a2f
mourdani <mourdani@student.42.fr>
2021-11-15 07:22:11 +0100

    add ft_puterr

--- sources/libft_utils.c
+++ sources/libft_utils.c
@@ -40,6 +40,18 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	return (0);
 }
 
+void	ft_puterr(char const *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+	{
+		write(2, &s[i], 1);
+		i++;
+	}
+}
+
 void	ft_putstr(char const *s)
 {
 	size_t	i;
